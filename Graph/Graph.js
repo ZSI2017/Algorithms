2ad6c4b27b0da89ef504be1a6528f5a940831bee
ZSI2017 <13407121461@163.com>
2017-10-17 00:05:20 +0800

    提交： 最短路径 广度优先搜索

--- Graph/Graph.js
+++ Graph/Graph.js
@@ -7,6 +7,7 @@
 
 function Graph(v) {
    this.vertices = v;
+   this.vertexList = [];
    this.edges = 0;
    this.adj = [];
    for(var i = 0; i<this.vertices;++i) {
@@ -19,6 +20,13 @@ function Graph(v) {
    // 广度优先搜索
    this.bfs = bfs;
    this.marked = [];
+   this.edgeTo = [];    // 最短路径；
+
+   this.hasPathTo = hasPathTo;
+   this.pathTo = pathTo;
+   this.topSortHelper = topSortHelper;
+   this.topSort = topSort;
+
    for(var i = 0;i<this.vertices;++i) {
      this.marked[i] = false;
    }
@@ -61,15 +69,62 @@ function bfs(s) {
   queue.push(s);      // 添加到队尾
   while(queue.length > 0) {
      var v = queue.shift();   // 从队首移除
-     if(v == undefined){
+     if(v !== ''){
          console.log("Visisted vertex:  "+v);
      }
      for(var w in this.adj[v]) {
         if(!this.marked[this.adj[v][w]]) {
-          //  this.edgeTo[this.adj[v][w]] = v;
+           this.edgeTo[this.adj[v][w]] = v;
            this.marked[this.adj[v][w]] = true;
            queue.push(this.adj[v][w]);
         }
      }
   }
+}
+
+// pathTo 创建的一个栈 保存与指定顶点有共同边的所有顶点，
+function pathTo(v) {
+    var source = 0;
+    if(!this.hasPathTo(v)) {
+        return undefined;
+    }
+    var path = [];
+    for(var i =v;i!=source;i=this.edgeTo[i]) {
+         path.push(i);
+    }
+    path.push(source);
+    return path;
+}
+
+function hasPathTo(v) {
+   return this.marked[v];
+}
+
+// 实现拓扑排序
+function topSort() {
+  var stack = [];
+  var visited = [];
+  for(var i =0;i<this.vertices;i++) {
+     visited[i] = false;
+  }
+  for(var i =0;i<this.vertices;i++){
+     if(visited[i] == false) {
+        this.topSortHelper(i,visited,stack);
+     }
+  }
+  for(var i =0;i<stack.length;i++) {
+     if(stack[i] != undefined && stack[i] != false) {
+        console.log(this.vertexList[stack[i]]);
+     }
+  }
+}
+
+function topSortHelper(v,visited,stack) {
+   visited[v] = true;
+   for(var i in this.adj[v]){
+       if(!visited[this.adj[v][w]]) {
+          this.topSortHelper(visited[this.adj[v][w]],visited,stack);
+       }
+   }
+   stack.push(v);
 }
