26a821a7126d5db7280a7239db5113df0024c4d3
ZSI2017 <13407121461@163.com>
2017-10-16 19:15:33 +0800

    lq 1016

--- Graph/Graph.js
+++ Graph/Graph.js
@@ -15,6 +15,13 @@ function Graph(v) {
    }
    this.addEdge = addEdge;
    this.showGraph = showGraph;
+   this.dfs = dfs;
+   // 广度优先搜索
+   this.bfs = bfs;
+   this.marked = [];
+   for(var i = 0;i<this.vertices;++i) {
+     this.marked[i] = false;
+   }
 }
 
 function addEdge(v,w) {
@@ -32,4 +39,37 @@ function showGraph(){
          }
        }
    }
+}
+
+// 使用深度优先搜索的方式来 遍历图
+function dfs(v) {
+    this.marked[v] = true;
+   if(this.adj[v] != undefined) {
+      console.log("Visited vertex: "+ v);
+   }
+   for(var w in this.adj[v]) {
+      if(!this.marked[this.adj[v][w]]) {
+          this.dfs(this.adj[v][w])
+      }
+   }
+}
+
+// 广度优先搜索， 一层一层的查找 图中的每个相邻 的节点
+function bfs(s) {
+  var queue = [];
+  this.marked[s] = true;
+  queue.push(s);      // 添加到队尾
+  while(queue.length > 0) {
+     var v = queue.shift();   // 从队首移除
+     if(v == undefined){
+         console.log("Visisted vertex:  "+v);
+     }
+     for(var w in this.adj[v]) {
+        if(!this.marked[this.adj[v][w]]) {
+          //  this.edgeTo[this.adj[v][w]] = v;
+           this.marked[this.adj[v][w]] = true;
+           queue.push(this.adj[v][w]);
+        }
+     }
+  }
 }
