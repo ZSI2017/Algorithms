60965bb0fa53334d2b1a329d7518f984d78deec4
ZSI2017 <13407121461@163.com>
2017-10-24 23:39:49 +0800

    lq 1024

--- title4.js
+++ title4.js
@@ -1,56 +1,75 @@
-var filList = [{
-  name: 'a.html',
-  path: 'src/app'
-}, {
-  name: 'b.html',
-  path: 'src/app'
-}, {
-  name: 'c.html',
-  path: 'app/com'
-}, {
-  name: 'd.html',
-  path: 'acc/com'
-}, {
-  name: 'e.html',
-  path: 'acc/com/app'
-}];
-function Node(title,children) {
-   this.title = title;
-   this.children = children;
+// var filList = [{
+//   name: 'a.html',
+//   path: 'src/app'
+// }, {
+//   name: 'b.html',
+//   path: 'src/app'
+// }, {
+//   name: 'c.html',
+//   path: 'app/com'
+// }, {
+//   name: 'd.html',
+//   path: 'acc/com'
+// }, {
+//   name: 'e.html',
+//   path: 'acc/com/app'
+// }];
+// function Node(title,children) {
+//    this.title = title;
+//    this.children = children;
+// }
+//
+// function tree() {
+//      this.root = [];
+// }
+//
+// function insert(filList) {
+//      filList.forEach(function(items,idx){
+//           var pathArr = items.path.split("/");
+//           // if(pathArr[0])
+//           pathArr.forEach(function(item,index){
+//                if(index !== pathArr.length-1) {
+//                   if(!this.findOne(item)){
+//                      var tempNode = new Node()
+//                   }
+//                }
+//           })
+//      })
+// }
+//
+// function findOne(titleName) {
+//   return (this.root.some(function(item,idx){
+//           return this.find(item,titleName)
+//     }))
+// }
+//
+// function find(obj,titleName) {
+//    if(obj.title === titleName) {
+//      return true;
+//    } else {
+//       if(typeof obj.children === "object"){
+//           obj.children.some(function(item,idx){
+//                 this.find(item,titleName);
+//           })
+//       }
+//    }
+// }
+
+var http = require('http');
+// http.get("https://www.baidu.com", function(res) {
+//  console.log("Got response: " + res.statusCode);
+//  }).on('error', function(e) {
+//  console.log("Got error: " + e.message);
+// });
+var urls = ['http://www.runoob.com/', 'http://www.hubwiz.com/', 'http://www.saylove.com/']
+function fetchPage(url) {
+   console.log(url);
+    http.get(urls[url], function(res) {
+
+        console.log("已经成功请求",url)
+    })
 }
 
-function tree() {
-     this.root = [];
-}
-
-function insert(filList) {
-     filList.forEach(function(items,idx){
-          var pathArr = items.path.split("/");
-          // if(pathArr[0])
-          pathArr.forEach(function(item,index){
-               if(index !== pathArr.length-1) {
-                  if(!this.findOne(item)){
-                     var tempNode = new Node()
-                  }
-               }
-          })
-     })
-}
-
-function findOne(titleName) {
-  return (this.root.some(function(item,idx){
-          return this.find(item,titleName)
-    }))
-}
-
-function find(obj,titleName) {
-   if(obj.title === titleName) {
-     return true;
-   } else {
-      if(typeof obj.children === "object"){
-          obj.children.some(function(item,idx){
-                this.find(item,titleName);
-          })
-      }
-   }
+for(var i = 0;i < urls.length;i++) {
+    fetchPage(i)
 }
